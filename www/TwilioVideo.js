de79e348cc9795773dbb285b42e709c61a700519
rpanadero <rpanadero@okode.com>
2018-09-14 12:47:24 +0200

    Setting config as optional argument that will be NULL if it is not defined

--- www/TwilioVideo.js
+++ www/TwilioVideo.js
@@ -3,6 +3,7 @@ var exec = require('cordova/exec');
 var TwilioVideo = function() {};
 
 TwilioVideo.openRoom = function(token, room, eventCallback, config) {
+    config = config != null ? config : null;
     exec(function(e) {
         console.log("Twilio video event fired: " + e);
         if (eventCallback) {

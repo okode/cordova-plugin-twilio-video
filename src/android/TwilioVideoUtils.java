b55e8cda15dcd878a6e025fb0ec12a63da00e998
Rubén Panadero <rpanadero@okode.com>
2020-06-12 13:57:39 +0200

    fix: twilio video error description

--- src/android/TwilioVideoUtils.java
+++ src/android/TwilioVideoUtils.java
@@ -16,7 +16,7 @@ public class TwilioVideoUtils {
         try {
             data = new JSONObject();
             data.put("code", String.valueOf(e.getCode()));
-            data.put("description", e.getExplanation());
+            data.put("description", e.getLocalizedMessage());
         } catch (JSONException e1) {
             Log.e(TwilioVideo.TAG, "Error converting Twilio exception to JSON", e);
         }
